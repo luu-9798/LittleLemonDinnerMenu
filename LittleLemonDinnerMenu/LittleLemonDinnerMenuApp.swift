7132e2a8e822afa44a902b7874b01e5f2aa1146c
luu-9798 <trungluu9798@gmail.com>
2025-05-14 16:13:38 -0400

    Initial Commit

---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenuApp.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Trung Luu on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
