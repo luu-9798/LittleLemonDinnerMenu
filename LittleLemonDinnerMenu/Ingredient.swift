2be050d45a87c4260f10eac2c8a8db63dcc74f49
luu-9798 <trungluu9798@gmail.com>
2025-05-14 17:08:57 -0400

    Define your enums - Ingredient.swift

---
+++ LittleLemonDinnerMenu/Ingredient.swift
@@ -0,0 +1,16 @@
+//
+//  Ingredient.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by Trung Luu on 5/14/25.
+//
+
+import Foundation
+
+enum Ingredient: String, CaseIterable {
+    case spinach     = "Spinach"
+    case broccoli    = "Broccoli"
+    case carrot      = "Carrot"
+    case pasta       = "Pasta"
+    case tomatoSauce = "Tomato Sauce"
+}
