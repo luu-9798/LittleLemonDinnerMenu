62cd64b6ac1c63dcff9bdc6a2a295a9706272f1f
luu-9798 <trungluu9798@gmail.com>
2025-05-14 17:42:30 -0400

    Unit tests (MenuItemTests.swift)

--- LittleLemonDinnerMenuTests/LittleLemonDinnerMenuTests.swift
+++ LittleLemonDinnerMenuTests/LittleLemonDinnerMenuTests.swift
@@ -10,27 +10,32 @@ import XCTest
 
 final class LittleLemonDinnerMenuTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMenuItemInitialization() {
+        let ingredients: [Ingredient] = [.pasta, .tomatoSauce]
+        let item = MenuItem(
+            title: "Pasta",
+            price: 14.0,
+            category: .food,
+            ordersCount: 0,
+            ingredients: ingredients
+        )
+
+        XCTAssertEqual(item.title, "Pasta")
+        XCTAssertEqual(item.price, 14.0)
+        XCTAssertEqual(item.category, .food)
+        XCTAssertEqual(item.ingredients, ingredients)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testOrdersCountMutation() {
+        var item = MenuItem(
+            title: "Bruschetta",
+            price: 8.5,
+            category: .food,
+            ordersCount: 0,
+            ingredients: [.tomatoSauce]
+        )
+        item.ordersCount += 1
+        XCTAssertEqual(item.ordersCount, 1)
     }
 
 }
